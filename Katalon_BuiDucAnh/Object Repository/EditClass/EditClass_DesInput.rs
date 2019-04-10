651da15c06d5ec00b965ed9f05214aa4c3785ef7
mathiznogoud <mathiznogoud@gmail.com>
2019-04-10 14:08:15 +0700

    Add files via upload

---
+++ Katalon_BuiDucAnh/Object Repository/EditClass/EditClass_DesInput.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>EditClass_DesInput</name>
+   <tag></tag>
+   <elementGuidId>501c6dec-4546-4b6b-b98e-e83c751c885c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[3]/div/div/form/div/div[3]/textarea</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[3]/div/div/form/div/div[3]/textarea</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Katalon_BuiDucAnh/Object Repository/EditClass/EditClass_NameInput.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>EditClass_NameInput</name>
+   <tag></tag>
+   <elementGuidId>cbe4ba99-d204-4f7e-b80b-32ad35b03e1c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>(//input[@type='text'])[3]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>(//input[@type='text'])[3]</value>
+   </webElementProperties>
+</WebElementEntity>
